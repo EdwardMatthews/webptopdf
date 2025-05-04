51e1eb58eab3d0aabeaf33b465ae73f70fc29cf7
wangjinzhen <149102935+tattooidea@users.noreply.github.com>
2025-05-05 01:24:00 +0800

    add r page and add to sitemap

---
+++ src/app/r/page.tsx
@@ -0,0 +1,14 @@
+export default function R() {
+    return (
+      <div>
+        <a href="https://gist.github.com/EdwardMatthews/53af73b9bd7963e344fae2de3e7aaf84">Gist Tool Recommendations</a>
+        <br />
+        <a href="https://feetgenerator.net/">Feet Generator</a>
+        <br />
+        <a href="https://cal.com/matthews-edward-qf14b4">Calendar</a>
+        <br />
+        <a href="https://www.creem.io/bip/edward">Creem</a>
+      </div>
+    );
+  }
+  
